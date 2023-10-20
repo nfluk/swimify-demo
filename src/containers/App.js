4771a0399979f2a53889da7d3daa3c8c56884e2c
nfluk <nickylukic@gmail.com>
2023-10-20 18:52:25 +0200

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h3">Hello World</Typography>
+    </div>
+  );
+}
+
+export default App;
